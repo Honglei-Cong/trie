63266eed3ddb9afa3e940440b51a75432e1e1cb1
conghonglei <conghonglei@gmail.com>
2018-09-11 11:51:06 +0800

    update crypto

--- common/types.go
+++ common/types.go
@@ -25,8 +25,8 @@ import (
 	"reflect"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/crypto/sha3"
+	"github.com/9thchain/trie/common/hexutil"
+	"github.com/9thchain/trie/crypto/sha3"
 )
 
 const (
