a7deaca03d819e20331e07179502e8966e75b110
conghonglei <conghonglei@gmail.com>
2018-09-12 18:40:18 +0800

    update main

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 
 package main
 
+import (
+	"github.com/9thchain/trie/db"
+	"github.com/9thchain/trie/common"
+	"github.com/9thchain/trie/trie"
+	"fmt"
+)
+
 func main() {
 
+	diskdb, _ := db.NewMemDatabase()
+	trie, _ := trie.New(common.Hash{}, trie.NewDatabase(diskdb))
+
+	trie.Update([]byte("a"), []byte("a-test-string"))
+	trie.Update([]byte("b"), []byte("b-test-string"))
+
+	root := trie.Hash()
+	fmt.Println("root hash:", common.Bytes2Hex(root.Bytes()))
 }
